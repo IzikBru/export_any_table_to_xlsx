2d1d0c1dcc242cc48cf8667c3e0d6395be87e6cd
itzikwei <itzik.bruchim@weizmann.ac.il>
2023-05-21 16:36:23 +0300

    changing naming to JS convention

--- export_table_to_xlsx.js
+++ export_table_to_xlsx.js
@@ -1,79 +1,79 @@
 /*  this function is used to export any given table to xlsx file.
-    table_dom - Required. The selector of the table DOM element (you can get it with: $("table:has(tbody)"))
-	skip_columns_indexes - Optional. In case some colums are not needed, they can be skipped by the yindex (starting from 0)
-    export_name - Optional. The name of the exported file (suffix .xlsx will be added)
+    tableDom - Required. The selector of the table DOM element (you can get it with: $("table:has(tbody)"))
+	skipColumnsIndexes - Optional. In case some colums are not needed, they can be skipped by the yindex (starting from 0)
+    exportName - Optional. The name of the exported file (suffix .xlsx will be added)
     important! to use this function, include the following code in your HTML header:
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.6/xlsx.core.min.js"></script> */
-function download_table_as_xlsx(table_dom, skip_columns_indexes = [], export_name = 'export') { 
+function downloadTableAsXLSX(tableDom, skipColumnsIndexes = [], exportName = 'export') { 
 	// check that the library was added to the HTML header
     if (typeof XLSX === "undefined") {
         alert("Need to include the xlsx library in the HTML header");
         return;
     }
-    // check that the table is not empty. another case could be an incorrect table selector ("table_dom")
-    if (table_dom === undefined) {
-        alert('There is no data to export. The table is empty');
+    // check that the table is not empty. another case could be an incorrect table selector ("tableDom")
+    if (!tableDom) {
+        alert('There is no data to export. The table is empty1');
         return;
     }
-	var rows_len = table_dom.rows.length;
-    if (rows_len < 2) {
-        alert('There is no data to export. The table is empty');
+	var rowsLen = tableDom.rows.length;
+    if (rowsLen < 2) {
+        alert('There is no data to export. The table is empty2');
         return;
     }    
 
-	// check that skip_columns_indexes contains only numbers
-	if (skip_columns_indexes.some(element => isNaN(element))) {
-		alert('The array ""skip_columns_indexes" comprised of non-number elements');
+	// check that skipColumnsIndexes contains only numbers
+	if (skipColumnsIndexes.some(element => isNaN(element))) {
+		alert('The array "skipColumnsIndexes" contains non-number elements');
 		return;
 	}
 
-	// preapre array of columns indexes (from 0...columns_length), and remove columns indexes that marked to be skipped
-	const column_indexes = Array.from(table_dom.rows[0].cells).map((_, index) => index)
-        .filter(column_index => !skip_columns_indexes.includes(column_index));
+	// preapre an array of column indexes (from 0...columns_length), and remove column indexes that are marked to be skipped
+	const columnIndexes = Array.from(tableDom.rows[0].cells).map((_, index) => index)
+        .filter(columnIndex => !skipColumnsIndexes.includes(columnIndex));
 
 	// populate a 2-dimensional array with the cell values
     const matrix = [];
-    for (let row_i = 0; row_i < rows_len; row_i++) {
-        matrix[row_i] = [];
-        let matrix_column_i = 0;
-        for (const column_i of column_indexes) {
-            const cell_dom = table_dom.rows[row_i].cells[column_i];
+    for (let rowIndex = 0; rowIndex < rowsLen; rowIndex++) {
+        matrix[rowIndex] = [];
+        let matrixColumnIndex = 0;
+        for (const columnIndex of columnIndexes) {
+            const cellDom = tableDom.rows[rowIndex].cells[columnIndex];
             // if the cell consists of inner DOM element(s), check the child(ren) element(s) with recursion fucntion
-            const cell_value = get_inner_cell_value(cell_dom);
-            matrix[row_i][matrix_column_i] = cell_value;
-            matrix_column_i++;			
+            const cellValue = getInnerCellValue(cellDom);
+            matrix[rowIndex][matrixColumnIndex] = cellValue;
+            matrixColumnIndex++;			
         }
     }
     // export the created 2D array to an xlsx file
     const worksheet = XLSX.utils.aoa_to_sheet(matrix);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-    XLSX.writeFile(workbook, export_name + ".xlsx");
+    XLSX.writeFile(workbook, exportName + ".xlsx");
 }
 
 /* recursive function that gets text of all elements in the hierarchy of a cell */
-function get_inner_cell_value(dom) {
+function getInnerCellValue(dom) {
     let text = '';
-    const dom_tag = dom.tagName.toLowerCase();
+    const domTag = dom.tagName.toLowerCase();
     
     // ignore the values of hidden items
     if (dom.type === 'hidden' || dom.style.display === 'none') {
         return '';
     }
     
-    // select can't have children so return its value
-    if (dom_tag === 'select') {
+    // <select> can't have children so return its value
+    if (domTag === 'select') {
         text = dom.options[dom.selectedIndex].text + ' ';
         return text;
     }
     
-    // for span/td/a elements, get their text content and continue
-    if (dom_tag === 'span' || dom_tag === 'td') {
-		const own_node = dom.childNodes;
-		if (own_node.length > 0) {
-			var own_text = dom.childNodes[0].nodeValue;
-			if (own_text) {
-				text += own_text + ' ';
+    // for <span>/<td> elements, get their text content and continue
+    if (domTag === 'span' || domTag === 'td') {
+		const ownNode = dom.childNodes;
+		if (ownNode.length > 0) {
+			var ownText = dom.childNodes[0].nodeValue;
+			if (ownText) {
+				text += ownText + ' ';
 			}
 		}   
     }
@@ -81,20 +81,20 @@ function get_inner_cell_value(dom) {
     // then concatenate the values of children elements
     
     // the "if" section uses recursion for elements with children, to get text of all the descendents 
-    var cell_children = dom.children;
-	if (cell_children.length > 0) { 
-        for (let i = 0; i < cell_children.length; i++) {
-            text += get_inner_cell_value(cell_children[i]);
+    const cellChildren = dom.children;
+	if (cellChildren.length > 0) { 
+        for (let i = 0; i < cellChildren.length; i++) {
+            text += getInnerCellValue(cellChildren[i]);
         }
     } else {
 		// if this element does not have children then return its text when available
-        if (dom_tag === 'th') {
+        if (domTag === 'th') {
             text = dom.textContent + ' ';
         }
-        else if (dom_tag === 'input') {
+        else if (domTag === 'input') {
             text = dom.value + ' ';
         }
-		else if (dom_tag === 'a') {
+		else if (domTag === 'a') {
             text = dom.textContent + ' ';
         }
         // in case of any another unique tag
